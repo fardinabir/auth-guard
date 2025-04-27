ce8fedcdbd12c05e63672605b6e85aa2bc4218c4
fardinabir <fardinabir@gmail.com>
2025-04-28 08:14:00 +0900

    merged updates from devlop

---
+++ controllers/users/api.go
@@ -0,0 +1,38 @@
+package users
+
+import (
+	"github.com/fardinabir/auth-guard/db/repos"
+	"github.com/fardinabir/auth-guard/service"
+	"github.com/go-chi/chi/v5"
+)
+
+type UserResource struct {
+	Users UserStore
+}
+
+func NewResource() *UserResource {
+	userStore := repos.NewUserStore()
+	return &UserResource{Users: userStore}
+}
+
+func (rs *UserResource) Router() *chi.Mux {
+	r := chi.NewRouter()
+	r.Use()
+
+	r.Mount("/users", rs.userRouter())
+	r.Post("/login", rs.Login)
+	r.Get("/", rs.HomePage)
+	return r
+}
+
+func (rs *UserResource) userRouter() *chi.Mux {
+	r := chi.NewRouter()
+	r.Use(service.NativeApiSecurity)
+	r.Get("/{id:[0-9]+}", rs.ReadUser)
+	r.Get("/", rs.ReadUsers)
+	r.Post("/", rs.CreateUser)
+	r.Patch("/{id:[0-9]+}", rs.UpdateUser)
+	r.Delete("/{id:[0-9]+}", rs.DeleteUser)
+
+	return r
+}
